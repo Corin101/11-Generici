69edf7a83a7f597b3ed84d7e18e87c00dd281ced
Marijo Horvat <Marijo.Horvat@vsite.hr>
2016-12-15 21:09:19 +0100

    kolekcije sa zivotinjama

--- GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
+++ GenerickeKolekcijeSRazlicitimTipovima/GeneričkeKolekcijeSRazličitimTipovima.cs
@@ -8,17 +8,17 @@ namespace Vsite.CSharp
     {
         public static void KretanjeŽivotinja()
         {
-            // TODO: U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
-            // TODO: Klasu ArrayList nadomjestiti generičkom listom Array<T>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
-            ArrayList živine = new ArrayList();
+            //  U datoteci Životinja.cs definirati apstraktnu klasu Životinja s apstraktnom (dakle virtualnom!) metodom void PokreniSe(). Proglasiti tu klasu baznom klasom za klase Gmaz, Ptica, Riba i Sisavac te prilagoditi potpise njihovih metoda PokreniSe.
+            //  Klasu ArrayList nadomjestiti generičkom listom Array<T>. Otkomentirati naredbu u petlji, prevesti program i provjeriti da nema pogrešaka.
+            List<Životinja> živine = new List<Životinja>();
             živine.Add(new Ptica());
             živine.Add(new Gmaz());
             živine.Add(new Sisavac());
             živine.Add(new Riba());
 
-            foreach (var živina in živine)
+            foreach (var živ in živine)
             {
-                // živina.PokreniSe();
+                živ.PokreniSe();
             }
         }
 
